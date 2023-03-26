e6ee2a0b2b1921d2bb58ee4c33708b3db07641e7
OpposedDeception <113610915+OpposedDeception@users.noreply.github.com>
2023-03-26 22:39:53 +0200

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,6 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
 import argparse
+import praw
 from rich.console import Console
 from rich.panel import Panel
 import pyfiglet
@@ -12,30 +11,36 @@ class RedditTracker:
         self.user = user
         self.upvotes = upvotes
         self.comments = comments
+        self.reddit = None
+
+    def authenticate(self, client_id, client_secret):
+        self.reddit = praw.Reddit(
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent="RedditTracker"
+        )
 
     def run(self):
         console = Console()
         app_name = Panel("Reddit Tracker", style="bold magenta")
         console.print(app_name)
-        url = f"https://www.reddit.com/user/{self.user}"
-        response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
-        soup = BeautifulSoup(response.content, "lxml")
-        display_name = soup.find("span", class_="_2BMnT8pWnIhEjqIWuJHYYa").text
-        karma_score = soup.find("span", class_="_1hNyZs13xy-JWvaI8OZ-3k").text
+        
+        user = self.reddit.redditor(self.user)
+        display_name = user.name
+        karma_score = user.link_karma + user.comment_karma
         console.print(f"\n[bold]{display_name}[/bold] ({karma_score} karma)")
-        subreddit_url = f"https://www.reddit.com/{self.subreddit}"
-        subreddit_response = requests.get(subreddit_url, headers={'User-agent': 'Mozilla/5.0'})
-        subreddit_soup = BeautifulSoup(subreddit_response.content, "lxml")
-        subreddit_name = subreddit_soup.find("span", class_="_2yYKzH1JjpzT3TENaSMSUz").text
-        subreddit_subscribers = subreddit_soup.find("span", class_="_1a4ZBXR9fNz5ttm8hpYvO4").text
+        
+        subreddit = self.reddit.subreddit(self.subreddit)
+        subreddit_name = subreddit.display_name
+        subreddit_subscribers = subreddit.subscribers
         console.print(f"\n[bold]Subreddit:[/bold] {subreddit_name} ({subreddit_subscribers} subscribers)")
 
         if self.upvotes:
-            upvote_count = soup.find("span", class_="_2KJtLbLCZJ0x2HsENFS8Da").text
+            upvote_count = user.link_karma
             console.print(f"\n[bold]{display_name}[/bold] has [bold]{upvote_count}[/bold] upvotes.")
 
         if self.comments:
-            comment_count = soup.find("span", class_="_2KJtLbLCZJ0x2HsENFS8Da", title="comment karma").text
+            comment_count = user.comment_karma
             console.print(f"\n[bold]{display_name}[/bold] has [bold]{comment_count}[/bold] comments.")
 
 
@@ -55,13 +60,23 @@ if __name__ == '__main__':
              ' V        V'
 """ + "\033[0m")
 
-    ver = pyfiglet.figlet_format("V.0.3")
+    ver = pyfiglet.figlet_format("V1.0")
     print(ver)
+
+    print("""
+          Provide these information to continue. To get necessary data
+          go to old Reddit and go to your account and click Developers settings.
+    """)
+    client_id = input("Enter your Reddit client ID: ")
+    client_secret = input("Enter your Reddit client secret: ")
+    
     parser = argparse.ArgumentParser(description="Reddit Tracker")
-    parser.add_argument("--reddit", "-r", help="Specify the subreddit name (e.g., r/learnpython)", required=True)
-    parser.add_argument("--user", "-u", help="Specify the Reddit user (e.g., u/Spez)", required=True)
+    parser.add_argument("--reddit", "-r", help="Specify the subreddit name (e.g., learnpython)", required=True)
+    parser.add_argument("--user", "-u", help="Specify the Reddit user (e.g., spez)", required=True)
     parser.add_argument("--upvotes", "-up", action="store_true", help="Get the number of upvotes for the user")
     parser.add_argument("--comments", "-c", action="store_true", help="Get the number of comments for the user")
     args = parser.parse_args()
+
     reddit_tracker = RedditTracker(args.reddit, args.user, args.upvotes, args.comments)
+    reddit_tracker.authenticate(client_id, client_secret)
     reddit_tracker.run()
