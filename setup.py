2f9041bda6a0553a705143626402f84f1a4d7752
OpposedDeception <113610915+OpposedDeception@users.noreply.github.com>
2023-04-09 20:56:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reddit-tracker',
-    version='1.1',
+    version='1.2',
     author='OpposedDeception',
     description='A console app for tracking Reddit users and subreddits',
     py_modules=['reddit_tracker'],
